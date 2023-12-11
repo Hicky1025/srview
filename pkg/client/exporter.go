154acf482b01d8a99c16f15efa1ab90507a53918
Hicky1025 <plane1025.ict@gmail.com>
2023-12-11 23:34:43 +0900

    first commit

--- pkg/client/exporter.go
+++ pkg/client/exporter.go
@@ -38,19 +38,26 @@ func (e *Exporter) Run(raddr *net.UDPAddr, flowChan chan []ipfix.FieldValue) err
 		fvs := <-flowChan
 		var sets []ipfix.Set
 
-		// 1. データセットの作成
+		// 1. データテンプレートの作成
 		var fss []ipfix.FieldSpecifier
 		for _, fv := range fvs {
 			fss = append(fss, *fv.FieldSpecifier())
 		}
+		tempRec := ipfix.NewTemplateRecord(e.tempRecSeq, fss)
+		tempSet := ipfix.NewSet(ipfix.TEMPLATE_SETS_ID, []ipfix.Record{tempRec})
+		sets = append(sets, *tempSet)
+
+		// 2. データの挿入
 		dataRec := &ipfix.DataRecord{FieldValues: fvs}
+		dataSet := ipfix.NewSet(e.tempRecSeq, []ipfix.Record{dataRec})
 		sets = append(sets, *dataSet)
 
-		// 2. メッセージとインクリメント・シークエンスの作成
+		// 3. メッセージの挿入
 		m = ipfix.NewMessage(e.flowSeq, OBSERVATION_ID, sets)
+		e.tempRecSeq += uint16(len(tempSet.Records))
 		e.flowSeq += uint32(len(dataSet.Records))
 
-		// 3. メッセージの送信
+		//4. 送信
 		SendMessage(m, conn)
 	}
 }
